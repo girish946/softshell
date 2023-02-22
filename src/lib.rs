bba1e0cd94fce8557138f1da7191b3214e2e6347
Girish Joshi <girish946@gmail.com>
2023-02-22 17:40:39 +0530

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+use pyo3::prelude::*;
+use sys_info;
+
+/// Returns the distribution name along with package manager
+#[pyfunction]
+fn get_dist_info() -> PyResult<String> {
+    let dist_release_info = sys_info::linux_os_release().unwrap();
+    let dist_id = dist_release_info.id.unwrap(); 
+    println!("os release is :{dist_id}");
+    if dist_id.eq("fedora"){
+        return Ok(dist_id + "dnf");
+    }
+    Ok(dist_id)
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn softshell(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(get_dist_info, m)?)?;
+    Ok(())
+}
