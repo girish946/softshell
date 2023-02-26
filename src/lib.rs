2848ce0870aeb603431971bb1ce5b81bb82aa3c7
Girish Joshi <girish946@gmail.com>
2023-02-26 11:43:07 +0530

    added  get_info functions
    Signed-off-by: Girish Joshi <girish946@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,58 @@
 use pyo3::prelude::*;
+
+use std::env;
 use sys_info;
+use which::which;
+
+/// returns the package manager for macos.
+fn get_macos_info() -> String {
+    match which("brew"){
+        Ok(_brew) => return String::from("macos:brew"),
+        Err(_) => {
+            println!("brew not found. Please visit https://brew.sh/ to install it.");
+            return String::from("macos");
+        }
+    }
+}
+
+/// returns the package info about linux distro
+fn get_linux_info() -> String {
+    let dist_release_info = sys_info::linux_os_release().unwrap();
+    let dist_id = dist_release_info.id.unwrap();
+    println!("os release is :{dist_id}");
+    if dist_id.eq("fedora") {
+        match which("dnf") {
+            Ok(_dnf) => return dist_id + ":dnf",
+            Err(_) => return dist_id
+        };
+    }
+    if dist_id.eq("ubuntu") {
+        return dist_id + ":apt";
+    }
+    dist_id
+}
+
+/// returns the package manager for windows 
+fn get_windows_info()-> String {
+    match which("winget.exe"){
+        Ok(_winget) => return String::from("windows:winget"),
+        Err(_) => {
+            println!("winget not found.");
+            println!("Please visit https://github.com/microsoft/winget-cli, to install it.");
+            return String::from("windows");
+        }
+    }
+}
 
 /// Returns the distribution name along with package manager
 #[pyfunction]
 fn get_dist_info() -> PyResult<String> {
-    let dist_release_info = sys_info::linux_os_release().unwrap();
-    let dist_id = dist_release_info.id.unwrap(); 
-    println!("os release is :{dist_id}");
-    if dist_id.eq("fedora"){
-        return Ok(dist_id + ":dnf");
+    match env::consts::OS {
+        "linux" => Ok(get_linux_info()),
+        "macos" => Ok(get_macos_info()),
+        "windows" => Ok(get_windows_info()),
+        _ => Ok(String::from("unsupported distro")),
     }
-    if dist_id.eq("ubuntu"){
-        return Ok(dist_id + ":apt");
-    }
-    Ok(dist_id)
 }
 
 /// A Python module implemented in Rust.
